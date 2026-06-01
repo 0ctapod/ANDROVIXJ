39b00b29731b2bfed3d4a550bc2be4d9f34d17ed
0ctapod <octaseevio@gmail.com>
2026-06-01 05:57:39 -0500

    Frontend listo para Deploy, API conectada

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,6 +4,6 @@
 // Debe cargarse ANTES que components.js y cualquier otro script.
 // =========================================================
 
-const API_URL      = "http://localhost:8080";
+const API_URL      = "https://androvixj-backend.onrender.com";
 const CLAVE_TOKEN  = "androvix_token";
 const CLAVE_SESION = "androvix_sesion";
